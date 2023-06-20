6f34f53554a08800949de36e550c5963d61d9026
claire-1125 <seojungbin0106@gmail.com>
2023-06-20 14:12:02 +0900

    chore: remove EDA and streamlit part

--- Frontend/[incomplete]EDA,streamlit/contents/__init__.py
+++
@@ -1,6 +0,0 @@
-from contents.home import HomeTab
-from contents.keyword import KeywordTab
-from contents.ingredient import IngredientTab
-from contents.dashboard import DashboardTab
-from contents.login import LoginTab
-from contents.recipe import RecipePage
